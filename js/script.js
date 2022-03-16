6b4b9832d09591f3e158ff17b3205bef2f116c6c
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-16 20:19:18 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Emmanuel Fofeyin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emmanuel.Fofeyin-Ngwamunwi
+// Created on: Sep 2022
 // This file contains the JS functions for index.html
 
 /**
